0c6537bf93ebb7a32b084da19eb28567f80902b8
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-12-30 11:30:30 +0200

    OpenAI method with Env variable works

--- backend/src/main/java/org/example/Controller/OpenAIController.java
+++ backend/src/main/java/org/example/Controller/OpenAIController.java
@@ -9,6 +9,21 @@ import java.util.ArrayList;
 
 public class OpenAIController {
 
+    public static void main(String[] args) {
+        ArrayList<String> reviews = new ArrayList<>();
+        reviews.add("Such an amazing experience! At one point I was looking through the drink book( I say book because it’s at least 20 pages!) and was struggling to find the by the glass wine list, an employee noticed and was by my side asking if I needed any help in seconds. Our server was super knowledgeable and was able to make several spot on recommendations. It was my 13 yr old daughter’s first experience at a restaurant like this and she was blown away. I told her before hand things like notice how when the servers reach in front of you, their face will always be facing you, in between each course they will clear out silverware and bring us the exact utensils we need for our next course. Etc. When we arrived, she was looking for all of those things and she kept saying dad look how they cleared out plates, they were always facing us and cleared our  plates all at the same time! Thanks for making her experience incredible. It’s the little things. Not to mention the food was off the charts. Such thoughtful pairing of flavor profiles and textures in each dish. Must try!");
+        ArrayList<String> AIresponse = getAIReviews(reviews);
+
+        System.out.println("Response 1");
+        System.out.println(AIresponse.get(0));
+
+        System.out.println("Response 2");
+        System.out.println(AIresponse.get(1));
+
+        System.out.println("Response 3");
+        System.out.println(AIresponse.get(2));
+    }
+
     public static ArrayList<String> getAIReviews(ArrayList<String> reviews) {
         ArrayList<String> AIReviews = new ArrayList<>();
 
@@ -25,7 +40,7 @@ public class OpenAIController {
 
     public static String chatGPT(String message) {
         String url = "https://api.openai.com/v1/chat/completions";
-        String apiKey = "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"; // API key goes here
+        String apiKey = System.getenv("openAI"); // API key goes here
         String model = "gpt-3.5-turbo"; // current model of chatgpt api
 
         try {
